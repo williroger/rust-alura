82512af88aa061a8667703ed6410cfa2d8633907
Roger <williroger@usp.br>
2023-11-26 23:24:29 -0300

    sintaxe basica

--- main.rs
+++ main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Ola mundo!");
+fn soma(a:i32, b:i32) -> i32 {
+    println!("{} + {} = {}", a, b, a+b);
+    a + b //a expresao que quero retornar sem o ponto e virgula
+}
+
+fn escopo() {
+   // println!("Ola mundo!");
+    let variavel:i32 = 300;
+    println!("variavel = {}, tamanho = {} bytes", variavel, std::mem::size_of_val(&variavel));
+    let a = 123;
+    fn  segundo_escopo(){
+        let a = 456;
+        println!("Var a do segundo Escopo = {}", a)
+    }
+    println!("Var a do escopo = {}", a);
+    segundo_escopo();
+}
+fn main(){
+    escopo();
+    println!("Soma = {}", soma(2, 3));
 }
